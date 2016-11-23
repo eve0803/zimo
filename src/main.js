2136ec1ae735657170146d14035e7bae41dce31a
wanyoung <wanyoung0123@gmail.com>
2016-11-23 23:34:54 +0800

    完成入口文件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+import App from './App'
+import routes from './routes'
+
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
 })
+
+router.beforeEach((to, from, next) => {
+  let token = window.localStorage.getItem('token')
+  if (to.matched.some(record => record.meta.requiresAuth) && (!token || token === null)) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+})
+
+export default new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
