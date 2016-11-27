528040d3c73ca70250f23ee616dcc2349f7d5247
wanyoung <wanyoung0123@gmail.com>
2016-11-27 22:35:10 +0800

    添加新的路由

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
-import Home from './components/Home'
-import Login from './components/Login'
+import Home from './views/Home'
+import Login from './views/Login'
+import Doc from './components/Doc'
 
 export default [
   {
+    name: 'home',
     path: '/',
-    component: Home
+    component: Home,
+    children: [
+      {
+        name: 'doc',
+        path: 'docs/:group/:path',
+        component: Doc
+      }
+    ]
   },
   {
+    name: 'login',
     path: '/login',
     component: Login
   }
